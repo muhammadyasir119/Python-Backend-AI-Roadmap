d2e2ea779fb568fdb2715da8fe705dd1ed71f88f
Muhammad yasir <yasirrashid125@gmail.com>
2026-08-01 21:17:33 +0500

    Add Main.py with utility function calls

---
+++ python-basics/week-1/datatypes/Week-3/Main.py
@@ -0,0 +1,15 @@
+# main.py
+
+import utils
+
+# greet function
+print(utils.greet("Ali"))
+
+# addition
+print("Addition:", utils.add(5, 3))
+
+# multiplication
+print("Multiply:", utils.multiply(4, 2))
+
+# square
+print("Square:", utils.square(6))
