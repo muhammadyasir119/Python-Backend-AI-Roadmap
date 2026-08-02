cb69ffb3a465eaba513df52dc02596522ac1b384
Muhammad yasir <yasirrashid125@gmail.com>
2026-08-02 21:32:24 +0500

    Add example usage of math and random modules
    Demonstrates usage of math and random modules with examples.

---
+++ python-basics/week-1/datatypes/Week-3/Modules.py
@@ -0,0 +1,21 @@
+# Week 3 - Day 2
+
+import math
+import random
+
+# 🔹 MATH MODULE
+
+print("Square root:", math.sqrt(25))
+print("Power:", math.pow(2, 3))
+print("Pi value:", math.pi)
+
+
+# 🔹 RANDOM MODULE
+
+print("Random number (1-10):", random.randint(1, 10))
+
+names = ["Ali", "Ahmed", "Yasir"]
+print("Random name:", random.choice(names))
+
+# Random float (0 to 1)
+print("Random float:", random.random())
